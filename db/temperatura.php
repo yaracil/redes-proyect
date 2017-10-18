e019f0ac6801b33ab05e969e6992df107b00673e
Yoe <yhacaribe@gmail.com>
2017-10-17 23:27:08 -0500

    last changes

---
+++ db/temperatura.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$bd = new SQLite3('example.db');
+$data_points = array();
+$results = $bd->query('SELECT date,temperature FROM arduino ORDER BY date DESC LIMIT 10');
+
+while ($row = $results->fetchArray()) {
+    $point = array("fecha"=> $row['0'], "temperatura" => $row['1']);
+    array_push($data_points, $point);
+}
+header("Content-type:application/json");
+echo json_encode($data_points);
+
+
+/*echo json_encode([
+    "one" => "Singular sensation",
+    "two" => "Beady little eyes",
+    "three" => "Little birds pitch by my doorstep"
+]);*/
+
+?>
