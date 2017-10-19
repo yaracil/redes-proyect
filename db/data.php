7ef9ca73fc002efb491fa72d586664489d9e7b18
Yoe <yhacaribe@gmail.com>
2017-10-19 15:19:15 -0500

    Design

--- db/data.php
+++ db/data.php
@@ -1,25 +1,20 @@
 <?php
 
 $db = new SQLite3('arduino.db', SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
-$db->exec("CREATE TABLE IF NOT EXISTS temperature (PatternID INTEGER PRIMARY KEY AUTOINCREMENT, temperature TEXT, Date TEXT)");
-$db->exec("CREATE TABLE IF NOT EXISTS heart_rate (PatternID INTEGER PRIMARY KEY AUTOINCREMENT, heart_rate TEXT, Date TEXT)");
-$db->exec("CREATE TABLE IF NOT EXISTS humidity (PatternID INTEGER PRIMARY KEY AUTOINCREMENT, humidity TEXT, Date TEXT)");
-
 if (true) {
     //datos de prueba
     ini_set('date.timezone', 'America/Mexico_City');
     $now = date("Y-m-d H:i:s");
     $i = 0;
     $resul = 0;
+    $db->exec("CREATE TABLE IF NOT EXISTS arduino (PatternID INTEGER PRIMARY KEY AUTOINCREMENT, heart_rate TEXT, temperature TEXT, humidity TEXT, date TEXT);");
     while ($i < 30) {
         $temp = rand(3, 40);
         $ritm = rand(0, 5);
         $hum = rand(10, 100);
 
-        if ($db->exec("INSERT INTO temperature (temperature, date) VALUES ('$temp', '$now')") == 1)
+        if ($db->exec("INSERT INTO arduino (heart_rate, temperature, humidity, date) VALUES ('$ritm', '$temp', '$hum', '$now')") == 1)
             $resul++;
-        $db->exec("INSERT INTO heart_rate (heart_rate, date) VALUES ('$ritm', '$now')");
-        $db->exec("INSERT INTO humidity (humidity, date) VALUES ('$hum', '$now')");
         $now = date("Y-m-d H:i:s", mktime(date('s') + $i));
         $i++;
     }
