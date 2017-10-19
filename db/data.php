e68d218bb66d02698068088872bac802f59722cf
Yoe <yhacaribe@gmail.com>
2017-10-19 16:51:38 -0500

    DB connection

--- db/data.php
+++ db/data.php
@@ -8,15 +8,16 @@ if (true) {
     $i = 0;
     $resul = 0;
     $db->exec("CREATE TABLE IF NOT EXISTS arduino (PatternID INTEGER PRIMARY KEY AUTOINCREMENT, heart_rate TEXT, temperature TEXT, humidity TEXT, date TEXT);");
-    while ($i < 30) {
+    while ($i < 50) {
         $temp = rand(3, 40);
-        $ritm = rand(0, 5);
+        $ritm = rand(1, 5);
         $hum = rand(10, 100);
 
         if ($db->exec("INSERT INTO arduino (heart_rate, temperature, humidity, date) VALUES ('$ritm', '$temp', '$hum', '$now')") == 1)
             $resul++;
         $now = date("Y-m-d H:i:s", mktime(date('s') + $i));
         $i++;
+        sleep(1);
     }
 }
 $db->close();
